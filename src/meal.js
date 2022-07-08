2260b6cf51d183939af86d38b2372a344350041c
Leah Young <leah.young21@alumni.colostate.edu>
2022-07-07 20:48:16 -0600

    Added up to formatPrice

--- src/meal.js
+++ src/meal.js
@@ -2,20 +2,33 @@ function nameMenuItem(items) {
 return `Delicious ${items}`
 }
 
-function createMenuItem(name, price, type) {
-  name: "French Toast",
-  price: 10.99,
-  type:"breakfast",
+function createMenuItem(itemName, itemPrice, itemType) {
+  return {
+    name: itemName,
+    price: itemPrice,
+    type: itemType
+    }
   }
+// Looking at the function "addIngredients", we include the parameters
+// "foodItem" and "ingredients" and then as the test ran through the if statement
+// each 
+function addIngredients (foodItem, ingredients) {
+  if (ingredients.includes(foodItem)) {
+    ingredients.pop();
+  }
+  ingredients[ingredients.length] = foodItem;
 }
 
+function formatPrice(price) {
+  return `$` + price;
 
+}
 
 module.exports = {
   nameMenuItem,
   createMenuItem,
-  // addIngredients,
-  // formatPrice,
+  addIngredients,
+  formatPrice,
   // decreasePrice,
   // createRecipe
 }
