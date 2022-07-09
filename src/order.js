2e32dda443314876487f46e271c83ff2609a6c2f
Leah Young <leah.young21@alumni.colostate.edu>
2022-07-09 09:38:04 -0600

    second order

--- src/order.js
+++ src/order.js
@@ -1,7 +1,24 @@
+function takeOrder(newOrder, deliveryOrders) {
+if (deliveryOrders.length < 3) {
+  return deliveryOrders.push(newOrder)
+  }
+}
+
+function refundOrder(orderNum, deliveryOrders) {
+for (var i = 0; i < deliveryOrders.length; i++) {
+  if (deliveryOrders[i].orderNumber === orderNum) {
+    return deliveryOrders.splice(i, 1);
+    }
+  }
+}
+
+
+
+
 
 module.exports = {
-  // takeOrder,
-  // refundOrder,
+  takeOrder,
+  refundOrder,
   // listItems,
   // searchOrder
 }
