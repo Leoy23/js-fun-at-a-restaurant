757c2e49af3073038a848806f73b801fb1f7c5ff
Leah Young <leah.young21@alumni.colostate.edu>
2022-07-10 12:06:22 -0600

    Update restaurant answers

--- src/restaurant.js
+++ src/restaurant.js
@@ -1,7 +1,35 @@
+function createRestaurant(name) {
+  var pizzaRestaurant = {
+    name,
+    menus: {
+      breakfast: [],
+      lunch: [],
+      dinner: [],
+    }
+  }
+  return pizzaRestaurant
+}
+
+
+function addMenuItem(pizzaRestaurant, menuItem) {
+  if (menuItem.type === "lunch" && !pizzaRestaurant.menus.lunch.includes(menuItem)) {
+    pizzaRestaurant.menus.lunch.push(menuItem)
+  }
+  else if (menuItem.type === "breakfast" && !pizzaRestaurant.menus.breakfast.includes(menuItem)) {
+    pizzaRestaurant.menus.breakfast.push(menuItem)
+ }
+  else if(menuItem.type === "dinner" && !pizzaRestaurant.menus.dinner.includes(menuItem)) {
+    pizzaRestaurant.menus.dinner.push(menuItem)
+  }
+}
+
+function removeMenuItem() {
+  
+}
 
 
 module.exports = {
-  // createRestaurant, 
-  // addMenuItem,
-  // removeMenuItem
+  createRestaurant,
+  addMenuItem,
+  removeMenuItem
 }
