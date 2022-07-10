d19addffbb073fea0a0595060ba7bb534a4fb4a6
Leah Young <leah.young21@alumni.colostate.edu>
2022-07-09 23:21:12 -0600

    Add final order.js

--- src/order.js
+++ src/order.js
@@ -12,13 +12,32 @@ for (var i = 0; i < deliveryOrders.length; i++) {
   }
 }
 
+function listItems(deliveryOrders) {
+  var itemList = []
+  for (var i = 0; i < deliveryOrders.length; i++ ) {
+    itemList.push(deliveryOrders[i].item)
+  }
+    return itemList.join(", ");
+}
 
+function searchOrder(deliveryOrders, orderItem) {
+  var order = false
+  for (var i = 0; i < deliveryOrders.length; i++) {
+    if (orderItem === deliveryOrders[i].item) {
+      order = true
+    }
+  }
+    return order;
+  }
+  // what am I trying to accomplish? Return or console.log a true or false value
+   // How? Through an "if" statement and a for loop
+   // what info do I need to access? the deliveryOrders array // using .includes() to result in true or false
 
 
 
 module.exports = {
   takeOrder,
   refundOrder,
-  // listItems,
-  // searchOrder
+  listItems,
+  searchOrder
 }
